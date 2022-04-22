12fef6edfd862f006c4c9b9a974814b5604b66c7
Jayesh Gohel <jayesh.gohel@gmail.com>
2022-04-22 22:33:23 +0530

    Fix deploy repository

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:piyush-shah/reservoir-watch.git main:gh-pages
+git push -f git@github.com:gnatih/ibp-design.git main:gh-pages
 cd -
